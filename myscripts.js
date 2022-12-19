36ccd9a60e6dc48a50e8821db2e9973e81cb6acd
aaryn <94torres.a@gmail.com>
2022-12-18 18:09:58 -0700

    added quotes array to JS file

---
+++ myscripts.js
@@ -0,0 +1,26 @@
+let quote = [
+  "I am pleased with my work.",
+  "I am brave and strong.",
+  "I am focused and concentrated.",
+  "I radiate warmth and light.",
+  "I am a good thinker.",
+  "I am intuitive.",
+  "I am expressive of my knowledge.",
+  "I am safe and secure.",
+  "I am filled with peace and calm.",
+  "I am confident in myself.",
+  "I am courageous and brave.",
+  "I am filled with positive energy.",
+  "I am thinking clearly.",
+  "I am relaxed and alert.",
+  "I am patient with myself.",
+  "I trust myself.",
+  "I am worthy of goodness.",
+  "I take care of myself as well as others.",
+  "I am accepting of the outcome.",
+  "I am expanding my knowledge every day.",
+  "I am doing my best, and my best is good enough.",
+];
+
+// let quote = document.getElementById("quote");
+// let btn = document.getElementById("btn");
