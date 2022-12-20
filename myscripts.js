3f7584f35595e3ed21552cc94aba55e5d9ce31f8
aaryn <94torres.a@gmail.com>
2022-12-19 19:47:46 -0700

    added onclick event to button

--- myscripts.js
+++ myscripts.js
@@ -1,8 +1,7 @@
-let quote = [
+const iAmAffirmations = [
   "I am pleased with my work.",
   "I am brave and strong.",
   "I am focused and concentrated.",
-  "I radiate warmth and light.",
   "I am a good thinker.",
   "I am intuitive.",
   "I am expressive of my knowledge.",
@@ -14,13 +13,14 @@ let quote = [
   "I am thinking clearly.",
   "I am relaxed and alert.",
   "I am patient with myself.",
-  "I trust myself.",
   "I am worthy of goodness.",
-  "I take care of myself as well as others.",
   "I am accepting of the outcome.",
   "I am expanding my knowledge every day.",
   "I am doing my best, and my best is good enough.",
 ];
 
-// let quote = document.getElementById("quote");
-// let btn = document.getElementById("btn");
+function generate() {
+  console.log("clicked");
+  let randomNumber = Math.floor(Math.random() * iAmAffirmations.length);
+  document.getElementById("quotes").innerHTML = iAmAffirmations[randomNumber];
+}
